5d20b2bfd702f162888d43e76709c3b1a6319bfe
youssefkhaldane <155687055+youssefkhaldane@users.noreply.github.com>
2024-03-01 12:47:55 +0100

    Add files via upload

---
+++ product.js
@@ -0,0 +1,23 @@
+// function changecolor() {
+//     document.getElementById("icons").classList.toggle("red-heart");
+// }
+
+// youssef code 
+const icons = document.querySelectorAll(".icons");
+// console.log(icons)
+function handleFavori(index){
+    // Get the reference to the element
+    var icon = icons[index];
+
+    // Toggle the "red-heart" class
+    icon.classList.toggle("red-heart");
+
+    // Set the base class "icons fa-solid fa-heart" if "red-heart" is not present, otherwise remove it
+    if (icon.classList.contains("red-heart")) {
+        console.log('im in')
+        icon.className = "icons fa-solid fa-heart red-heart";
+    }
+    else{
+        icon.className = "icons fa-regular fa-heart";
+    }
+}
